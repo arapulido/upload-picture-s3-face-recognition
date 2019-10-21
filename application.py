ae514a5fd4ad331e7cb1a0652f7533f012d2de7b
Ara Pulido <ara.pulido@datadoghq.com>
2019-10-21 10:11:25 +0200

    Initial commit

---
+++ application.py
@@ -0,0 +1,66 @@
+# IMPORTS
+from ddtrace import patch_all
+patch_all()
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import json
+import os
+import requests
+
+# CONFIG
+application = Flask(__name__, instance_relative_config=True)
+application.config.from_object(os.environ['APP_SETTINGS'])
+
+
+from tools import upload_file_to_s3
+
+ALLOWED_EXTENSIONS = application.config["ALLOWED_EXTENSIONS"]
+
+
+# ROUTES
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@application.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # There is no file selected to upload
+        if "user_file" not in request.files:
+            return "No user_file key in request.files"
+
+        if request.form['user_id'] == None:
+            return "Please, add a user name"
+
+        user_name = request.form['user_id']
+
+        file = request.files["user_file"]
+
+        # There is no file selected to upload
+        if file.filename == "":
+            return "Please select a file"
+
+        # File is selected, upload to S3 and show S3 URL
+        if file and allowed_file(file.filename):
+
+            data = {"srcBucket": application.config['S3_BUCKET'],
+            "name":file.filename, 
+            "userId": user_name} 
+
+            file.filename = secure_filename(file.filename)
+            output = upload_file_to_s3(file, application.config['S3_KEY'], application.config['S3_SECRET'], application.config["S3_BUCKET"])
+
+            return requests.post(application.config['FACE_DETECTION_ENDPOINT'],
+                data=json.dumps(data)).content
+    else:
+        return render_template("index.html")
+
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
---
+++ tools.py
@@ -0,0 +1,29 @@
+from ddtrace import patch_all
+patch_all()
+
+import boto3
+
+def upload_file_to_s3(file, bucket_name, aws_access_key_id, aws_secret_access_key, acl="public-read"):
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+
+    return "{}{}".format(bucket_name, file.filename)
